ac72ff1a5600f2a23fb2b26151db63395d79c8f4
Kovalski <mihaenko11@mail.ru>
2022-05-10 23:15:26 +0300

    Update admin.php

--- 6/admin.php
+++ 6/admin.php
@@ -179,7 +179,8 @@ if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW'])) {
                             $powers = $db->prepare("SELECT distinct name from powersowners join superpowers2 pow on power_id = pow.id where owner_id = ?");
                             $powers->execute(array($value['id']));
                             $superpowers = $powers->fetchAll(PDO::FETCH_ASSOC);
-                            echo implode(',', $superpowers);
+                            $superpowers = implode(',', $superpowers);
+                            echo $superpowers;
                             ?>
                         </td>
                         <td id="bio">
